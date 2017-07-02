f46a2ced88ccd2c19b9285bdcc6c955a0a0586cc
DESKTOP-72IS7O3\ferna <fernando@gammapartners.com>
2017-07-01 18:00:30 -0600

    .

--- messages/bot-msgs.js
+++ messages/bot-msgs.js
@@ -2,9 +2,9 @@ let msgs =
   {
     // Start sending offers
     'START_SENDING_OFFERS': '¡Genial! En cualquier momento comenzarás a recibir ofertas y promociones para ti',
-    'START_SENDING_OFFERS_BUTTON1': 'Ver mis ofertas de hoy',
+    'START_SENDING_OFFERS_BUTTON1': 'Ver ofertas de hoy',
     'START_SENDING_OFFERS_BUTTON2': 'Compartir bot',
     // Otros textos
   };
-  
+
 module.exports = msgs;
