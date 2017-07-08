a23f9f56005770ec1c536f5dabebf1b2f461ce29
Andre Contreras <andre.contreras@gammapartners.com>
2017-07-07 18:34:34 -0600

    fix issue with cors

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-Boteru-Password-Oytupw94nih");
     //intercepts OPTIONS method
     if ('OPTIONS' === req.method) {
