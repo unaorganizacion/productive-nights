1e26dc1db21b11c1b641750acc3867e9932c3658
Andre Contreras <andre.contreras@gammapartners.com>
2017-07-09 20:14:16 -0600

    Implementing carousel of today offers

--- today-offers/index.js
+++ today-offers/index.js
@@ -18,8 +18,8 @@ module.exports = function (datastore, userObject) {
 
     return new Promise((resolve, reject) => {
         let
-            today = moment().startOf('day').toDate(),
-            tomorrow = moment().endOf('day').toDate(),
+            today = moment().startOf('day'),
+            tomorrow = moment().endOf('day'),
             posts = []
         ;
 
@@ -35,7 +35,7 @@ module.exports = function (datastore, userObject) {
             return function (cb) {
                 console.log('querying for', category);
                 let query = datastore.createQuery("Post")
-                    .filter('category', category)
+                        .filter('categories', category)
                     //.filter('sentDate','>=', new Date(today))
                 ;
                 query.run((err, entities, info) => {
@@ -46,16 +46,15 @@ module.exports = function (datastore, userObject) {
                         return;
                     }
 
-                    console.log('entity from cat', category, entities);
+                    //console.log('entity from cat', category, entities);
                     for (let entity of entities) {
                         let post = fromDatastore(entity);
-                        if (entity.sentDate >= today &&
-                            entity.sentDate < tomorrow &&
-                            !checkDup(post))
-                        {
+                        let entitySentDate = moment(entity.sentDate);
+                        console.log(entitySentDate.format('YYYY-MM-DD'));
+                        if (!checkDup(post)) {
                             posts.push(post);
                         } else {
-                            console.log('dup or not in range');
+                            console.log('dup or not in range', entitySentDate.isSame(today, 'd'), !checkDup(post));
                         }
                     }
                     cb();
@@ -84,31 +83,31 @@ module.exports = function (datastore, userObject) {
 };
 
 /*[
-    {
-        title: "Carlin's gay",
-        subtitle: "Next-generation virtual reality",
-        item_url: "https://www.oculus.com/en-us/rift/",
-        image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-        buttons: [{
-            type: "web_url",
-            url: "https://goo.gl/maps/oguRzdieeyy",
-            title: "Ver ubicación"
-        }, {
-            type: "postback",
-            title: "Compartir anuncio",
-            payload: "Payload for first bubble",
-        }],
-    }, {
-        title: "touch",
-        subtitle: "Your Hands, Now in VR",
-        item_url: "https://www.oculus.com/en-us/touch/",
-        image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-        buttons: [{
-            type: "web_url",
-            url: "https://www.facebook.com/sharer/sharer.php?u=https%3A//goo.gl/maps/oguRzdieeyy",
-            title: "Share this con FB!"
-        }, {
-            type: "element_share",
-        }]
-    }
-];*/
+ {
+ title: "Carlin's gay",
+ subtitle: "Next-generation virtual reality",
+ item_url: "https://www.oculus.com/en-us/rift/",
+ image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+ buttons: [{
+ type: "web_url",
+ url: "https://goo.gl/maps/oguRzdieeyy",
+ title: "Ver ubicación"
+ }, {
+ type: "postback",
+ title: "Compartir anuncio",
+ payload: "Payload for first bubble",
+ }],
+ }, {
+ title: "touch",
+ subtitle: "Your Hands, Now in VR",
+ item_url: "https://www.oculus.com/en-us/touch/",
+ image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+ buttons: [{
+ type: "web_url",
+ url: "https://www.facebook.com/sharer/sharer.php?u=https%3A//goo.gl/maps/oguRzdieeyy",
+ title: "Share this con FB!"
+ }, {
+ type: "element_share",
+ }]
+ }
+ ];*/
