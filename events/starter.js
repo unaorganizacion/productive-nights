a61b0e0186907034160ddbca89e75f8d629153c4
Andre Contreras <andre.contreras@gammapartners.com>
2017-07-23 18:47:16 -0600

    Fixing X7b5MXSb

--- events/starter.js
+++ events/starter.js
@@ -13,7 +13,6 @@ event.prototype.setDatastore = function (datastore) {
 };
 
 event.prototype.run = function () {
-  //console.log("sending today event to", this.userObject.mId, this.datastore);
   let self = this;
   new Promise(function (resolve, reject) {
     sendMessage.sendTextMessage(self.userObject.mId, "¡Heyyyyyy! Mi nombre es Dillio y mi propósito es que nunca vuelvas a perderte de una gran oferta o promoción", [], resolve);
@@ -23,16 +22,13 @@ event.prototype.run = function () {
       sendMessage.sendTextMessage(self.userObject.mId, "Por ahora solo conozco las de Chihuahua, Chihuahua. ¡Pero pronto agregaré más ciudades :D!", [], resolve);
     });
   })
-  /* .then(function () {
-    return new Promise(function (resolve) {
-      sendMessage.sendTextMessage(self.userObject.mId, "Solo por haberme agregado te voy a regalar...", [], resolve);
-    });
-  }) */
   // todo: select random offer in the pool today-offers
   .then(function () {
     return new Promise(function (resolve) {
-      let categoriesQuickResponse = require("../tools/getCategoriesAsQuickReply")(self.userObject);
-      // console.log("quick replies", categoriesQuickResponse);
+      let
+        categories = require("../tools/compileCategories")(self.userObject.userData.interest, 1, self.userObject.userData.locale),
+        categoriesQuickResponse = require("../tools/getCategoriesAsQuickReply")(self.userObject, 1, categories)
+      ;
       sendMessage.sendTextMessage(self.userObject.mId, "¿Quieres enterarte de lo mejor y solo lo mejor? ¡Yo sé que sí! De las siguientes categorías selecciona las que sean de tu interés", categoriesQuickResponse, resolve);
     });
   })
