8a8ce8034d1874f124b4a1c6ef5cb92acb3b01ed
Andre Contreras <andre.contreras@gammapartners.com>
2017-07-24 20:51:01 -0600

    Fixing issues all days

--- tools/utilities.js
+++ tools/utilities.js
@@ -2,16 +2,18 @@ module.exports = {
   cleanUserInterests: function (interests) {
     let registeredInterests = [];
     if (interests.length > 0) {
-      for (let i in interests) {
-        if (interests.hasOwnProperty(i) && isNaN(interests[i])) {
-          delete interests[i];
-          continue;
-        }
-        if (interests.hasOwnProperty(i) && registeredInterests.indexOf(interests[i]) !== -1) {
-          delete interests[i];
-          continue;
-        }
-      }
+      try {
+          for (let i in interests) {
+              if (interests.hasOwnProperty(i) && isNaN(interests[i])) {
+                  delete interests[i];
+                  continue;
+              }
+              if (interests.hasOwnProperty(i) && registeredInterests.indexOf(interests[i]) !== -1) {
+                  delete interests[i];
+                  continue;
+              }
+          }
+      } catch (e) { console.log("error in clean user interest", e); }
     }
     
     return interests;
